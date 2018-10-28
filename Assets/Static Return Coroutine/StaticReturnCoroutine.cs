f18e83175692a4730f9ab897cbc5ea0e5627b1a4
Mr.True.China <mr.true.china@gmail.com>
2018-10-28 15:24:49 +0900

    static return Coroutian
    静态和返回值都有了

---
+++ Assets/Static Return Coroutine/StaticReturnCoroutine.cs
@@ -0,0 +1,76 @@
+﻿/*
+ *  把静态调用协程和协程返回写到一个脚本里...
+ *  
+ *  第一次有了静态调用协程的类
+ *  有了能从协程获得返回值的类
+ *  这两个类写在一起
+ *  怎么感觉这么的没技术呢
+ *  
+ *  就是给协程返回类加了一个构造方法，在不传 MonoBehaviour 的时候走静态调用
+ */
+
+using System.Collections;
+using UnityEngine;
+
+public class StaticReturnCoroutine : MonoBehaviour
+{
+    static StaticReturnCoroutine instance
+    {
+        get
+        {
+            if (_instance != null)
+                return _instance;
+
+            _instance = new GameObject("Static Return Coroutine Instance").AddComponent<StaticReturnCoroutine>();
+            return _instance;
+        }
+    }
+    static StaticReturnCoroutine _instance;
+
+
+    public static Coroutine Start(IEnumerator coroutine)
+    {
+        return instance.StartCoroutine(coroutine);
+    }
+
+
+    private void OnDestroy()
+    {
+        StopAllCoroutines();
+    }
+}
+
+
+public class ReturnCoroutine<T>
+{
+    public Coroutine coroutine
+    {
+        get { return _coroutine; }
+    }
+    Coroutine _coroutine;
+
+    public T result
+    {
+        get { return _result; }
+    }
+    T _result;
+
+
+    //就在这加了这么一个构造方法，不传 mono 就直接走静态，别的改变一点没有
+    public ReturnCoroutine(IEnumerator coroutine)
+    {
+        _coroutine = StaticReturnCoroutine.Start(Start(coroutine));
+    }
+    public ReturnCoroutine(MonoBehaviour mono, IEnumerator coroutine)
+    {
+        _coroutine = mono.StartCoroutine(Start(coroutine));
+    }
+
+
+    IEnumerator Start(IEnumerator coroutine)
+    {
+        while (coroutine.MoveNext())
+            yield return coroutine.Current;
+        _result = (T)coroutine.Current;
+    }
+}
---
+++ Assets/Static Return Coroutine/StaticReturnCoroutineExample.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class StaticReturnCoroutineExample : MonoBehaviour
+{
+    private void Start()
+    {
+        DoExample();
+    }
+
+    static void DoExample()
+    {
+        StaticReturnCoroutine.Start(StartWaitThreeSecondLog());
+        StaticReturnCoroutine.Start(StartWaitTwoSecondLog());
+        StaticReturnCoroutine.Start(StartWaitOneSecondLog());
+    }
+
+    static IEnumerator StartWaitThreeSecondLog()
+    {
+        ReturnCoroutine<string> bc = new ReturnCoroutine<string>(WaitThreeSecondLog());
+        yield return bc.coroutine;
+        string result = bc.result;
+
+        Debug.Log(result);
+    }
+
+    static IEnumerator StartWaitTwoSecondLog()
+    {
+        ReturnCoroutine<string> bc = new ReturnCoroutine<string>(WaitTwoSecondLog());
+        yield return bc.coroutine;
+        string result = bc.result;
+
+        Debug.Log(result);
+    }
+
+    static IEnumerator StartWaitOneSecondLog()
+    {
+        ReturnCoroutine<string> bc = new ReturnCoroutine<string>(WaitOneSecondLog());
+        yield return bc.coroutine;
+        string result = bc.result;
+
+        Debug.Log(result);
+    }
+
+    static IEnumerator WaitThreeSecondLog()
+    {
+        yield return new WaitForSeconds(3);
+        yield return "!";
+    }
+
+    static IEnumerator WaitTwoSecondLog()
+    {
+        yield return new WaitForSeconds(2);
+        yield return "World";
+    }
+
+    static IEnumerator WaitOneSecondLog()
+    {
+        yield return new WaitForSeconds(1);
+        yield return "Hellow";
+    }
+}
